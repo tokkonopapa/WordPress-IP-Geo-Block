62addab5cc168fe95ff5bec03e806bb956872f4f
tokkonopapa <tokkonopapa@yahoo.com>
2014-10-18 22:33:34 +0900

    avoid warnings

--- ip-geo-block/includes/writelog.php
+++ ip-geo-block/includes/writelog.php
@@ -9,32 +9,32 @@ function ip_geo_block_log( $ip, $hook, $validate ) {
 	$file = IP_GEO_BLOCK_PATH . "logs/log-${hook}.php";
 	$size = @filesize( $file );
 
-	$fp = @fopen( $file, "c+" );
-	if ( flock( $fp, LOCK_EX | LOCK_NB ) ) {
-		$lines = $size ? explode( "\n", fread( $fp, $size ) ) : array();
+	if ( $fp = @fopen( $file, "c+" ) ) {
+		if ( flock( $fp, LOCK_EX | LOCK_NB ) ) {
+			$lines = $size ? explode( "\n", fread( $fp, $size ) ) : array();
+			array_shift( $lines );
+			array_pop  ( $lines );
+			$lines = array_slice( $lines, -IP_GEO_BLOCK_LOG_LEN );
 
-		array_shift( $lines );
-		array_pop  ( $lines );
-		$lines = array_slice( $lines, -IP_GEO_BLOCK_LOG_LEN );
+			array_push(
+				$lines, 
+				sprintf( "%d,%s,%s,%s,%s\n",
+					time(),
+					$ip,
+					$validate['code'],
+					$_SERVER['HTTP_USER_AGENT'],
+					$_SERVER['REQUEST_URI']
+				)
+			);
 
-		array_push(
-			$lines, 
-			sprintf( "%d,%s,%s,%s,%s\n",
-				time(),
-				$ip,
-				$validate['code'],
-				$_SERVER['HTTP_USER_AGENT'],
-				$_SERVER['REQUEST_URI']
-			)
-		);
+			rewind( $fp );
+			ftruncate( $fp, 0 );
+			fwrite( $fp, "<?php/*\n" );
+			fwrite( $fp, implode( "\n", $lines ) );
+			fwrite( $fp, "*/?>" );
+			flock( $fp, LOCK_UN | LOCK_NB );
+		}
 
-		rewind( $fp );
-		ftruncate( $fp, 0 );
-		fwrite( $fp, "<?php/*\n" );
-		fwrite( $fp, implode( "\n", $lines ) );
-		fwrite( $fp, "*/?>" );
-		flock( $fp, LOCK_UN | LOCK_NB );
+		fclose( $fp );
 	}
-
-	fclose( $fp );
 }
