47844d833b23fa5339d39e592414be2f3039441c
tokkonopapa <tokkonopapa@yahoo.com>
2014-10-18 23:11:27 +0900

    add cookie into log file

--- ip-geo-block/includes/writelog.php
+++ ip-geo-block/includes/writelog.php
@@ -12,26 +12,26 @@ function ip_geo_block_log( $ip, $hook, $validate ) {
 	if ( $fp = @fopen( $file, "c+" ) ) {
 		if ( @flock( $fp, LOCK_EX | LOCK_NB ) ) {
 			$lines = $size ? explode( "\n", fread( $fp, $size ) ) : array();
+
 			array_shift( $lines );
 			array_pop  ( $lines );
 			$lines = array_slice( $lines, -IP_GEO_BLOCK_LOG_LEN );
 
 			array_push(
 				$lines, 
-				sprintf( "%d,%s,%s,%s,%s\n",
+				sprintf( "%d,%s,%s,%s,%s,%s\n",
 					time(),
 					$ip,
 					$validate['code'],
-					$_SERVER['HTTP_USER_AGENT'],
-					$_SERVER['REQUEST_URI']
+					$_SERVER['REQUEST_URI'],
+					$_SERVER['HTTP_USER_AGENT'], // should be sanitized
+					json_encode( $_COOKIE ) // should be sanitized
 				)
 			);
 
 			rewind( $fp );
 			ftruncate( $fp, 0 );
-			fwrite( $fp, "<?php/*\n" );
-			fwrite( $fp, implode( "\n", $lines ) );
-			fwrite( $fp, "*/?>" );
+			fwrite( $fp, "<?php/*\n" . implode( "\n", $lines ) . "*/?>" );
 			@flock( $fp, LOCK_UN | LOCK_NB );
 		}
 
