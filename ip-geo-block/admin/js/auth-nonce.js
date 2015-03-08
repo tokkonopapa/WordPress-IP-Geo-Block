262e26847fae2c7f250cf91228975b5bec1b7e11
tokkonopapa <tokkonopapa@yahoo.com>
2015-03-08 13:00:18 +0900

    Added check of core actions to authenticate.

--- ip-geo-block/admin/js/auth-nonce.js
+++ ip-geo-block/admin/js/auth-nonce.js
@@ -13,7 +13,7 @@
 	$(document).ajaxSend(function (event, jqxhr, settings) {
 		var nonce = $('meta[name=ip-geo-block-auth-nonce]').attr('content');
 		if (nonce && settings.url.indexOf('wp-admin/admin-ajax.php') >= 0) {
-			var data = settings.data.split('&');
+			var data = settings.data ? settings.data.split('&') : [];
 			data.push('ip-geo-block-auth-nonce=' + nonce);
 			settings.data = data.join('&');
 		}
