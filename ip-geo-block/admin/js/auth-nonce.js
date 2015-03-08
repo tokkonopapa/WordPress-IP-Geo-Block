7fdbbe88ee6d03a29e3342ce46a7076b27f3b44d
tokkonopapa <tokkonopapa@yahoo.com>
2015-03-08 14:15:33 +0900

    Remove ajaxSetup() because it is not recommended.

--- ip-geo-block/admin/js/auth-nonce.js
+++ ip-geo-block/admin/js/auth-nonce.js
@@ -4,18 +4,22 @@
  * <meta name="ip-geo-block-auth-nonce" content="..." />
  */
 (function ($) {
+//	$.ajaxSetup({
+//		beforeSend: function () {
+			$(document).ajaxSend(function (event, jqxhr, settings) {
+				var nonce = $('meta[name=ip-geo-block-auth-nonce]').attr('content');
+				if (nonce && settings.url.indexOf('wp-admin/admin-ajax.php') >= 0) {
+					var data = settings.data ? settings.data.split('&') : [];
+					data.push('ip-geo-block-auth-nonce=' + nonce);
+					settings.data = data.join('&');
+				}
+			});
+//		}
+//	});
 	$(function () {
 		var nonce = $('meta[name=ip-geo-block-auth-nonce]').attr('content');
 		if (nonce) {
 			$('form').append('<input type="hidden" name="ip-geo-block-auth-nonce" value="' + nonce + '" />');
-		}
-	});
-	$(document).ajaxSend(function (event, jqxhr, settings) {
-		var nonce = $('meta[name=ip-geo-block-auth-nonce]').attr('content');
-		if (nonce && settings.url.indexOf('wp-admin/admin-ajax.php') >= 0) {
-			var data = settings.data ? settings.data.split('&') : [];
-			data.push('ip-geo-block-auth-nonce=' + nonce);
-			settings.data = data.join('&');
 		}
 	});
 }(jQuery));
