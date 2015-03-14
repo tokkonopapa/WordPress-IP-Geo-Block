e695f9f7943c884b89e794e05eec72c511c6ca53
tokkonopapa <tokkonopapa@yahoo.com>
2015-03-15 02:14:34 +0900

    Added embeding nonce to link tag.

--- ip-geo-block/admin/js/auth-nonce.js
+++ ip-geo-block/admin/js/auth-nonce.js
@@ -19,12 +19,19 @@
 		};
 	}
 
+	function is_admin(url) {
+		return url.indexOf('wp-admin/admin-ajax.php') >= 0 ||
+		       url.indexOf('wp-admin/admin-post.php') >= 0 ? true : false;
+	}
+
+	function query_args(uri, args) {
+		var url = uri.scheme ? uri.scheme + '://' : '';
+		return url + uri.authority + uri.path + '?' + args.join('&');
+	}
+
 	$(document).ajaxSend(function (event, jqxhr, settings) {
 		var nonce = IP_GEO_AUTH.nonce || null;
-		if (nonce && (
-			settings.url.indexOf('wp-admin/admin-ajax.php') >= 0 ||
-			settings.url.indexOf('wp-admin/admin-post.php') >= 0
-		)) {
+		if (nonce && is_admin(settings.url)) {
 			// multipart/form-data
 			if (settings.data instanceof FormData) {
 				settings.data.append('ip-geo-block-auth-nonce', nonce);
@@ -40,9 +47,7 @@
 				if (typeof settings.data === 'undefined' || uri.query) {
 					data = uri.query ? uri.query.split('&') : [];
 					data.push('ip-geo-block-auth-nonce=' + encodeURIComponent(nonce));
-					settings.url  = uri.scheme ? uri.scheme + '://' : '';
-					settings.url += uri.authority + uri.path;
-					settings.url += '?' + data.join('&');
+					settings.url = query_args(uri, data);
 				} else {
 					data = settings.data ? settings.data.split('&') : [];
 					data.push('ip-geo-block-auth-nonce=' + encodeURIComponent(nonce));
@@ -55,6 +60,17 @@
 	$(function () {
 		var nonce = IP_GEO_AUTH.nonce || null;
 		if (nonce) {
+//			$('a').on('click', function(event) {
+			$('a').each(function(index, elem) {
+				var href = $(this).attr('href');
+				if (is_admin(href)) {
+					var data, uri = uri = parse_uri(href);
+					data = uri.query ? uri.query.split('&') : [];
+					data.push('ip-geo-block-auth-nonce=' + encodeURIComponent(nonce));
+					$(this).attr('href', query_args(uri, data));
+				}
+			});
+
 			$('form').append('<input type="hidden" name="ip-geo-block-auth-nonce" value="' + nonce + '" />');
 		}
 	});
