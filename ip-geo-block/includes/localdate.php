e77cbe293f709fa2ddc183f3f8919fb611f59e73
tokkonopapa <tokkonopapa@yahoo.com>
2015-04-14 00:31:16 +0900

    Support the case that date_default_timezone_set() is used.

--- ip-geo-block/includes/localdate.php
+++ ip-geo-block/includes/localdate.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * Return local time of date.
+ * Return local time of day.
  *
  */
 function ip_geo_block_localdate( $timestamp = FALSE, $format = NULL ) {
 	static $offset = NULL;
 	if ( $offset === NULL )
-		$offset = (int)( get_option( 'gmt_offset' ) * HOUR_IN_SECONDS );
+		$offset = ( 'UTC' === date_default_timezone_get() ?
+			(int)( get_option( 'gmt_offset' ) * HOUR_IN_SECONDS ) : 0 );
 
 	if ( ! $format )
 		$format = get_option( 'date_format' ) . ' ' . get_option( 'time_format' );
