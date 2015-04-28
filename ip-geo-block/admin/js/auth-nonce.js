fa57b61c9cdeac7a958bf4c0e994b343b0115c1c
tokkonopapa <tokkonopapa@yahoo.com>
2015-04-28 23:20:11 +0900

    Extend to embed nonce to wp-admin.

--- ip-geo-block/admin/js/auth-nonce.js
+++ ip-geo-block/admin/js/auth-nonce.js
@@ -31,7 +31,7 @@
 	function is_admin(url, query) {
 		var uri = parse_uri(url ? url.toString().toLowerCase() : ''),
 		    http = /https?/.test(uri.scheme),
-		    path = uri.path || location.pathname;
+		    path = uri.path.charAt(0) === '/' ? uri.path : location.pathname;
 
 		// explicit scheme and external domain
 		if (http && uri.authority !== location.host.toLowerCase()) {
@@ -39,10 +39,9 @@
 		}
 
 		// check scheme, path, query
-		return (http || !uri.scheme) && (
-			path.indexOf('admin.php'     ) >= 0 ||
-			path.indexOf('admin-ajax.php') >= 0 ||
-			path.indexOf('admin-post.php') >= 0
+		return (
+			// under the wp-admin
+			path.indexOf('/wp-admin/') >= 0
 		) && (
 			// if method is POST, then type of query is 'object'
 			typeof query === 'string' ? /(?:action)=/.test(query) : true
