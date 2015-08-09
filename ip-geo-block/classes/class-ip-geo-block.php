054c523d9b0fd3ef2004acec3d4984a6a035f5a7
tokkonopapa <tokkonopapa@yahoo.com>
2015-08-09 20:40:17 +0900

    Rename auth-nonce.js to authenticate.js as Autoptimize excludes this type of filename.

--- ip-geo-block/classes/class-ip-geo-block.php
+++ ip-geo-block/classes/class-ip-geo-block.php
@@ -208,7 +208,7 @@ class IP_Geo_Block {
 	public static function enqueue_nonce() {
 		if ( is_user_logged_in() ) {
 			$handle = self::PLUGIN_SLUG . '-auth-nonce';
-			$script = plugins_url( 'admin/js/auth-nonce.js', IP_GEO_BLOCK_BASE );
+			$script = plugins_url( 'admin/js/authenticate.js', IP_GEO_BLOCK_BASE );
 			$nonce = array( 'nonce' => wp_create_nonce( $handle ) ) + self::$content_dir;
 			wp_enqueue_script( $handle, $script, array( 'jquery' ), self::VERSION );
 			wp_localize_script( $handle, 'IP_GEO_BLOCK_AUTH', $nonce );
