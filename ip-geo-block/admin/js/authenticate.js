6b40f22b1aeeeafb6c45bd6901743386cf4ec4a4
tokkonopapa <tokkonopapa@yahoo.com>
2015-08-18 01:36:39 +0900

    Fix an error of parenthesis.

--- ip-geo-block/admin/js/authenticate.js
+++ ip-geo-block/admin/js/authenticate.js
@@ -2,7 +2,6 @@
  * WP-ZEP - Zero-day exploit Prevention for wp-admin
  *
  */
-
 // Polyfill of Array.prototype.indexOf()
 // https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
 Array.prototype.indexOf||(Array.prototype.indexOf=function(d,e){var a;if(null==this)throw new TypeError('"this" is null or not defined');var c=Object(this),b=c.length>>>0;if(0===b)return-1;a=+e||0;Infinity===Math.abs(a)&&(a=0);if(a>=b)return-1;for(a=Math.max(0<=a?a:b-Math.abs(a),0);a<b;){if(a in c&&c[a]===d)return a;a++}return-1});
@@ -134,8 +133,7 @@ var IP_GEO_BLOCK_ZEP = {
 			// avoid the case that is fragment (`#...`) only
 			if ((uri.scheme || uri.path || uri.query) && regexp.test(path) &&
 			    (!exclude || ajax_links.indexOf(path) < 0)) {
-					return 1; // internal for admin
-				}
+				return 1; // internal for admin
 			}
 		}
 
