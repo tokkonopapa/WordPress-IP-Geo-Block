2ecf1243235a04b6c5781c4cfacd113a75bba3b5
tokkonopapa <tokkonopapa@yahoo.com>
2015-11-02 09:14:31 +0900

    Delete PEAR.php because of deploy error.

--- ip-geo-block/includes/Net/IPv4.php
+++ ip-geo-block/includes/Net/IPv4.php
@@ -21,7 +21,15 @@
 * @link       http://pear.php.net/package/Net_IPv4
 */
 
-require_once 'PEAR.php';
+//require_once 'PEAR.php';
+class PEAR {
+	static raiseError( $msg ) {
+		return false;
+	}
+	static isError( $data, $msgcode ) {
+		return false === $data;
+	}
+}
 
 // {{{ GLOBALS
 /**
