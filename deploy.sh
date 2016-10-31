f44318e096eb4622ae937bccf5112c532a4a1147
tokkonopapa <tokkonopapa@yahoo.com>
2016-10-31 09:54:34 +0900

    Version 2.2.9

--- deploy.sh
+++ deploy.sh
@@ -136,7 +136,7 @@ cd $SVNPATH
 svn copy trunk/ tags/$NEWVERSION1/
 cd $SVNPATH/tags/$NEWVERSION1
 svn delete --force \
-	trunk/ \
+	trunk/
 svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
 
 # for assets
