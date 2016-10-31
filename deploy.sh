66f434edfe8a7c5ce1c0efecabd3b9893212da36
tokkonopapa <tokkonopapa@yahoo.com>
2016-10-31 09:35:09 +0900

    Version 2.2.9

--- deploy.sh
+++ deploy.sh
@@ -121,7 +121,7 @@ echo "Creating new SVN tag & committing it"
 cd $SVNPATH
 
 # Delete unused files
-#svn delete --force \
+svn delete --force \
 #	trunk/classes/class-ip-geo-block-api.php \
 #	trunk/includes/upgrade.php \
 #	trunk/admin/js/footable.all.min.js \
@@ -131,6 +131,8 @@ cd $SVNPATH
 #	trunk/includes/venders/ \
 #	trunk/includes/download.php \
 #	trunk/includes/localdate.php
+	tags/2.2.9/trunk/ \
+	tags/2.2.9/ip-geo-api/
 
 # Copy all files to tags
 svn copy trunk/ tags/$NEWVERSION1/
