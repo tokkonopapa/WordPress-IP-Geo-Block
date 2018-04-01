81058d9c81326999b2bb060534b981cf5ca098b0
tokkonopapa <tokkonopapa@yahoo.com>
2018-04-01 12:40:09 +0900

    Add option to retrieve all sites.

--- ip-geo-block/classes/class-ip-geo-block-util.php
+++ ip-geo-block/classes/class-ip-geo-block-util.php
@@ -935,7 +935,7 @@ class IP_Geo_Block_Util {
 	public static function get_multisite() {
 		$sites = array();
 
-		foreach ( get_blogs_of_user( get_current_user_id(), current_user_can( 'manage_options' ) ) as $site ) { // @since 3.0.0
+		foreach ( get_blogs_of_user( get_current_user_id(), current_user_can( 'manage_network_options' ) ) as $site ) { // @since 3.0.0
 			$sites[] = preg_replace( '/^https?:/', '', $site->siteurl );
 		}
 
