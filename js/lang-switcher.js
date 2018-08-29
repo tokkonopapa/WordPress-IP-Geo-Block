59a2851dc73962315ae498bafdc2ff375289c0ef
tokkonopapa <tokkonopapa@yahoo.com>
2018-08-30 00:12:39 +0900

    Update language switcher.

--- js/lang-switcher.js
+++ js/lang-switcher.js
@@ -1,7 +1,16 @@
+    // head.html
     (function(win, doc) {
-        var link = doc.getElementById('lang'), lang = (doc.cookie.replace(/(?:(?:^|.*;\s*)lang\s*\=\s*([^;]*).*$)|^.*$/, "$1"));// || ((win.navigator.userLanguage || win.navigator.language).indexOf('ja') !== -1 ? 'ja' : 'en'));
-        if (link && lang === link.hreflang && lang !== (-1 !== win.location.href.indexOf('-' + lang + '.') ? 'ja' : 'en')) {
+        var link = doc.getElementById('lang'),
+            page = doc.getElementById('canonical').href.indexOf('-ja.') !== -1 ? 'ja' : 'en',
+            lang = doc.cookie.replace(/(?:(?:^|.*;\s*)lang\s*\=\s*([^;]*).*$)|^.*$/, "$1") || ((win.navigator.userLanguage || win.navigator.language).indexOf('ja') !== -1 ? 'ja' : 'en');
+        if (link && page !== lang) {
             doc.cookie = 'lang=' + lang + '; path=/';
             win.location.href = link.href + win.location.hash;
         }
+    })(window, document);
+
+    // navi.html
+    (function(win, doc) {
+        var lang = doc.cookie.replace(/(?:(?:^|.*;\s*)lang\s*\=\s*([^;]*).*$)|^.*$/, "$1") || ((win.navigator.userLanguage || win.navigator.language).indexOf('ja') !== -1 ? 'ja' : 'en');
+        doc.getElementById('lang-switch').checked = 'ja' === lang;
     })(window, document);
