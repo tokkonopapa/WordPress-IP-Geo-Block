449dd113e05a4be914fe5a429076378d5b4dfb55
tokkonopapa <tokkonopapa@yahoo.com>
2018-08-30 23:13:18 +0900

    Fix the redirection loop on history back.

--- js/lang-switcher.js
+++ js/lang-switcher.js
@@ -3,16 +3,30 @@
         var link = doc.getElementById('lang'),
             page = doc.getElementById('canonical').href.indexOf('-ja.') !== -1 ? 'ja' : 'en',
             lang = doc.cookie.replace(/(?:(?:^|.*;\s*)lang\s*\=\s*([^;]*).*$)|^.*$/, "$1") || ((win.navigator.userLanguage || win.navigator.language).indexOf('ja') !== -1 ? 'ja' : 'en');
-        if (link && page !== lang) {
+        if (link && win.location.href.indexOf(link.href) < 0 && page !== lang) {
             doc.cookie = 'lang=' + lang + '; path=/';
             win.location.href = link.href + win.location.hash;
         }
     })(window, document);
 
+// Closure Compiler
+(function(a,b){var c=b.getElementById("lang"),e=-1!==b.getElementById("canonical").href.indexOf("-ja.")?"ja":"en",d=b.cookie.replace(/(?:(?:^|.*;\s*)lang\s*=\s*([^;]*).*$)|^.*$/,"$1")||(-1!==(a.navigator.userLanguage||a.navigator.language).indexOf("ja")?"ja":"en");c&&0>a.location.href.indexOf(c.href)&&e!==d&&(b.cookie="lang="+d+"; path=/",a.location.href=c.href+a.location.hash)})(window,document);
+
+// UglifyJS 3.0
+!function(a,e){var n=e.getElementById("lang"),o=-1!==e.getElementById("canonical").href.indexOf("-ja.")?"ja":"en",i=e.cookie.replace(/(?:(?:^|.*;\s*)lang\s*\=\s*([^;]*).*$)|^.*$/,"$1")||(-1!==(a.navigator.userLanguage||a.navigator.language).indexOf("ja")?"ja":"en");n&&a.location.href.indexOf(n.href)<0&&o!==i&&(e.cookie="lang="+i+"; path=/",a.location.href=n.href+a.location.hash)}(window,document);
+
     // navi.html
     (function(win, doc) {
         var lang = doc.cookie.replace(/(?:(?:^|.*;\s*)lang\s*\=\s*([^;]*).*$)|^.*$/, "$1") || ((win.navigator.userLanguage || win.navigator.language).indexOf('ja') !== -1 ? 'ja' : 'en');
         if ('ja' === lang) {
-            doc.getElementById('switch-container').classList.add('switch-on');
+            (doc.getElementById('switch-container').classList || {
+                add: function() {} // dummy function that does not support `classList` (IE9-)
+            }).add('switch-on');
         }
     })(window, document);
+
+// Closure Compiler
+!function(a,n){"ja"===(n.cookie.replace(/(?:(?:^|.*;\s*)lang\s*\=\s*([^;]*).*$)|^.*$/,"$1")||(-1!==(a.navigator.userLanguage||a.navigator.language).indexOf("ja")?"ja":"en"))&&(n.getElementById("switch-container").classList||{add:function(){}}).add("switch-on")}(window,document);
+
+// UglifyJS 3.0
+(function(a,b){"ja"===(b.cookie.replace(/(?:(?:^|.*;\s*)lang\s*=\s*([^;]*).*$)|^.*$/,"$1")||(-1!==(a.navigator.userLanguage||a.navigator.language).indexOf("ja")?"ja":"en"))&&(b.getElementById("switch-container").classList||{add:function(){}}).add("switch-on")})(window,document);
--- js/main.js
+++ js/main.js
@@ -1,86 +1,115 @@
-(function($) {
-	$(function() {
+(function(window, document, $) {
+	/**
+	 * Preferred language
+	 *
+	 */
+ 	var preferred = document.cookie.replace(/(?:(?:^|.*;\s*)lang\s*\=\s*([^;]*).*$)|^.*$/, "$1") || ((window.navigator.userLanguage || window.navigator.language).indexOf('ja') !== -1 ? 'ja' : 'en');
 
-		$('.button-collapse').sideNav();
+	/**
+	 * Disqus
+	 *
+	 */
+	function addDisqus(script) {
+		var dsq = document.createElement('script');
+		dsq.type = 'text/javascript';
+		dsq.async = true;
+		dsq.src = 'https://ip-geo-block' + (preferred === 'jp' ? '-jp' : '') + '.disqus.com/' + script;
+		dsq.setAttribute('data-timestamp', +new Date());
+		(document.head || document.body).appendChild(dsq);
+	}
 
-		$('#menu-close').on('click', function() {
-			$('#sidenav-overlay').trigger('click');
-		});
+	// navigation menu
+	$('.button-collapse').sideNav();
 
-		// language switcher
-		$('#switch-container').removeClass('switch-on'); // remove class added in navi.html
+	$('#menu-close').on('click', function() {
+		$('#sidenav-overlay').trigger('click');
+	});
 
-		var page = $('link[rel=canonical]').attr('href').indexOf('-ja.') !== -1 ? 'ja' : 'en',
-		    lang = document.cookie.replace(/(?:(?:^|.*;\s*)lang\s*\=\s*([^;]*).*$)|^.*$/, "$1") ||
-		           ((window.navigator.userLanguage || window.navigator.language).indexOf('ja') !== -1 ? 'ja' : 'en');
+	// language switcher
+	$('#switch-container').removeClass('switch-on'); // remove class added in navi.html
 
-		$('#lang-switch').prop('checked', lang === 'ja').on('change', function() {
-			var prop = $(this).prop("checked") ? 'ja' : 'en'; // false: En(left), true: Ja(right)
-			document.cookie = 'lang=' + prop + '; path=/';
-			if (prop !== page) {
-				lang = $('#' + ('ja' === prop ? 'lang' : 'lang-x')).attr('href');
-				if (lang) {
-					window.location.href = lang;
-				}
+	$('#lang-switch').prop('checked', 'ja' === preferred).on('change', function() {
+		var prop = $(this).prop("checked") ? 'ja' : 'en', // false: En(left), true: Ja(right)
+		    page = $('link[rel=canonical]').attr('href').indexOf('-ja.') !== -1 ? 'ja' : 'en';
+
+		// save cookie
+		document.cookie = 'lang=' + prop + '; path=/';
+
+		// redirect if current page is not preferred
+		if (page !== prop) {
+			page = $('#' + ('ja' === prop ? 'lang' : 'lang-x')).attr('href');
+			if (page) {
+				window.location.href = page;
 			}
-		}).trigger('change');
-
-		// consent
-		if (!document.cookie.replace(/(?:(?:^|.*;\s*)consent\s*\=\s*([^;]*).*$)|^.*$/, "$1")) {
-			$('.consent').addClass('consent-show');
 		}
 
-		$('.compliance').on('click', 'a', function() {
-			document.cookie = 'consent=gotit; max-age=31536000; path=/';
-			$('.consent').addClass('consent-hide');
-		});
-
-		$(window).on('load', function() {
-			var hash = window.location.hash || null;
-			if (hash) {
-				window.location.hash = '';
-				window.location.hash = hash;
+		// just kick off disqus
+		else {
+			var dsq = $('#disqus_thread');
+			if (dsq.length && location.hostname.indexOf('ipgeoblock') >= 0) {
+				dsq.empty();
+				addDisqus('embed.js');
+//				addDisqus('count.js');
 			}
+		}
+	}).trigger('change');
+
+	// consent
+	if (!document.cookie.replace(/(?:(?:^|.*;\s*)consent\s*\=\s*([^;]*).*$)|^.*$/, "$1")) {
+		$('.consent').addClass('consent-show');
+	}
+
+	$('.compliance').on('click', 'a', function() {
+		document.cookie = 'consent=true; max-age=31536000; path=/';
+		$('.consent').addClass('consent-hide');
+	});
+
+	// jump to the hash if it is specified
+	$(window).on('load', function() {
+		var hash = window.location.hash || null;
+		if (hash) {
+			window.location.hash = '';
+			window.location.hash = hash;
+		}
+	});
+
+	// Horizontal staggered list
+	Materialize.showScrolled = function(selector) {
+		var time = 0;
+		$(selector).find('p').velocity({
+			translateX: "-100px"
+		}, {
+			duration: 0
 		});
 
-		// Horizontal staggered list
-		Materialize.showScrolled = function(selector) {
-			var time = 0;
-			$(selector).find('p').velocity({
-				translateX: "-100px"
+		$(selector).find('p').each(function() {
+			$(this).velocity({
+				opacity: "1",
+				translateX: "0"
 			}, {
-				duration: 0
+				duration: 800,
+				delay: time,
+				easing: [60, 10]
 			});
+			time += 120;
+		});
+	};
 
-			$(selector).find('p').each(function() {
-				$(this).velocity({
-					opacity: "1",
-					translateX: "0"
-				}, {
-					duration: 800,
-					delay: time,
-					easing: [60, 10]
-				});
-				time += 120;
-			});
-		};
+	Materialize.scrollFire([{
+			selector: '#scroll-fire1',
+			offset: 50,
+			callback: 'Materialize.showScrolled("#scroll-fire1")'
+		},
+		{
+			selector: '#scroll-fire2',
+			offset: 50,
+			callback: 'Materialize.showScrolled("#scroll-fire2")'
+		},
+		{
+			selector: '#scroll-fire3',
+			offset: 50,
+			callback: 'Materialize.showScrolled("#scroll-fire3")'
+		}
+	]);
 
-		Materialize.scrollFire([{
-				selector: '#scroll-fire1',
-				offset: 50,
-				callback: 'Materialize.showScrolled("#scroll-fire1")'
-			},
-			{
-				selector: '#scroll-fire2',
-				offset: 50,
-				callback: 'Materialize.showScrolled("#scroll-fire2")'
-			},
-			{
-				selector: '#scroll-fire3',
-				offset: 50,
-				callback: 'Materialize.showScrolled("#scroll-fire3")'
-			}
-		]);
-
-	}); // end of document ready
-})(jQuery); // end of jQuery name space
+})(window, document, jQuery); // end of jQuery name space
